21ef74daac29eb4fadf92e4f028161963abe2eb2
Rustamov Khumoyun <122399191+humoyuntech@users.noreply.github.com>
2024-09-15 23:01:11 +0500

    Analytics page-2

--- src/components/Analytics/index.jsx
+++ src/components/Analytics/index.jsx
@@ -1,7 +1,9 @@
 import Subtitle from "../Generics/Subtitle"
 import Title from "../Generics/Title"
-import {Container, Card, Wrapper, Section, Plus, Counter, Arrow, SubCard} from "./style"
+import {Container, Card, Wrapper, Section, Plus, Counter, Arrow, SubCard, FooterWrapper} from "./style"
 import {privateData, media} from "../../utils/analitics"
+import Emai from "./Emai"
+import Moliya from "./Moliya"
 
 function Analytics() {
   return (
@@ -27,28 +29,38 @@ function Analytics() {
                 )
               })}
         </Wrapper>
-         <Subtitle mt={24} mb={16}>Ijtimoiy Tarmoqlar</Subtitle>
-         <Wrapper>
-         {media.map((value)=> {
-                const {icon: Icon} = value;
-                return (
-                <SubCard key={value.id} gap={24} title={value.title}>
-                  <Section title={value.title}>
-                    <Subtitle><Icon className="subicon" />{value.title}</Subtitle> {" "} <Plus title={value.title}/>
-                  </Section>
+        <Subtitle mt={24} mb={16}>Ijtimoiy Tarmoqlar</Subtitle>
+        <Wrapper>
+        {media.map((value)=> {
+              const {icon: Icon} = value;
+              return (
+              <SubCard key={value.id} gap={24} title={value.title}>
+                <Section title={value.title}>
+                  <Subtitle><Icon className="subicon" />{value.title}</Subtitle> {" "} <Plus title={value.title}/>
+                </Section>
 
-                  <Section title={value.title}>
-                    <Title color={"#52C41A"}>
-                      <Arrow/> 22%
-                    </Title>
-                    <Counter>{value.count}K</Counter>
-                  </Section>
-                </SubCard>
-                )
-              })}
-         </Wrapper>
+                <Section title={value.title}>
+                  <Title color={"#52C41A"}>
+                    <Arrow/> 22%
+                  </Title>
+                  <Counter>{value.count}K</Counter>
+                </Section>
+              </SubCard>
+              )
+            })}
+        </Wrapper>
+        <FooterWrapper>
+          <FooterWrapper.Email>
+            <Subtitle mt={24} mb={16} count={12}>Email habarlari</Subtitle>
+            <Emai/>
+          </FooterWrapper.Email>
+          <FooterWrapper.Moliya>
+            <Subtitle mt={24} mb={16} count={12}>Moliya</Subtitle>
+          <Moliya/>
+          </FooterWrapper.Moliya>
+        </FooterWrapper>
     </Container>
-  )
+  )                                       
 }
 
 export default Analytics
