2dd457270d10c1a68af6da40c702c31916ed1b78
Rustamov Khumoyun <122399191+humoyuntech@users.noreply.github.com>
2024-10-13 17:35:20 +0500

    Students Page

---
+++ src/components/Guruhlar/Guruhlar/style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import edit from "../../../assets/icons/edit.svg?react";
+import del from "../../../assets/icons/delete.svg?react";
+
+export const Container = styled.div``;
+export const Action = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  /* gap: 18px; */
+  z-index: 9999;
+`;
+
+Action.Edit = styled(edit)`
+  padding: 10px;
+  width: 42px;
+  height: 42px;
+`;
+Action.Move = styled(edit)`
+  padding: 10px;
+  width: 42px;
+  height: 42px;
+`;
+Action.Delete = styled(del)`
+  padding: 10px;
+  width: 42px;
+  height: 42px;
+`;
